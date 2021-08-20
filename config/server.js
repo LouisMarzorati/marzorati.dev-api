4bd5fc955995a0606dd52ca3d912a9683d3a5a3a
louis marzorati <louismarzorati@gmail.com>
2021-08-20 13:59:10 -0400

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '31221278bef08555143fd87607763b1f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '098af5df-1538-4fb6-b2cb-a60c79b5af8d'
+};
